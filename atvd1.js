d87fc260561615e2bcd15433bc800b47c37fdab9
CVMScris <102757901+CVMScris@users.noreply.github.com>
2022-05-16 14:18:56 -0300

    Add files via upload

---
+++ atvd1.js
@@ -0,0 +1,28 @@
+// 1-
+let b = 0
+while (b <= 100){
+    console.log(b);
+      b = b + 1
+}
+
+// 2-
+let a = 50
+while (a <= 100){
+    console.log(a);
+      a = a + 1
+}
+
+// 3-
+let g = 10
+while (g >= 0){
+    console.log(g);
+      g = g - 1
+}
+
+// 4-
+
+let f = 0
+while (f <= 20){
+    console.log(f);
+    f = f +2
+}
